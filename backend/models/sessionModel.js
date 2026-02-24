27210809ff07cae598757c958117b5ba65abf657
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2026-02-24 13:54:16 +0530

    empty

--- backend/models/sessionModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const sessionSchema = new mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-    }
-},{timestamps:true})
-export const Session = mongoose.model('Session',sessionSchema);
